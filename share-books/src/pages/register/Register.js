c306841db893be18261e94498fcccb06f2362f18
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-08-26 18:43:06 -0300

    added states in the inputs

--- share-books/src/pages/register/Register.js
+++ share-books/src/pages/register/Register.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 import './Register.css'
 
 const Register = () => {
+
+    const [displayName, setDisplayName] = useState("")
+    const [email, setEmail] = useState("")
+    const [birthday, setBirthday] = ("")
+    const [password, setPassword] = ("")
+    const [passwordConfirm, setPasswordConfirm] = ("")
 
     return (
         <div className='principal-div div-register'>
@@ -10,19 +17,49 @@ const Register = () => {
 
             <form className="form-register-page">
                 <label for="displayName">Nome completo</label>
-                <input type='text' id='displayName' placeholder='Nome completo' name='displayName'></input>
+                <input 
+                type='text' 
+                id='displayName' 
+                placeholder='Nome completo' 
+                name='displayName'
+                onChange={(e) => setDisplayName(e.target.value)}
+                ></input>
 
                 <label for="email">Email</label>
-                <input type='email' id='email' placeholder='Email' name='email'></input>
+                <input 
+                type='email' 
+                id='email' 
+                placeholder='Email' 
+                name='email'
+                onChange={(e) => setEmail(e.target.value)}
+                ></input>
 
                 <label for='birthday'>Data de nascimento</label>
-                <input type='text' id='birthday' placeholder='01/01/1999' name='birthday'></input>
+                <input 
+                type='text' 
+                id='birthday' 
+                placeholder='01/01/1999' 
+                name='birthday'
+                onChange={(e) => setBirthday(e.target.value)}
+                ></input>
 
                 <label for='password'>Senha</label>
-                <input type='password' id='password' placeholder='Senha' name='password'></input>
+                <input 
+                type='password' 
+                id='password' 
+                placeholder='Senha' 
+                name='password'
+                onChange={(e) => setPassword(e.target.value)}
+                ></input>
 
                 <label for='password-confirm'>Confirme sua senha</label>
-                <input type='password' id='password-confirm' placeholder='confirme sua senha' name='password-confirm'></input>
+                <input 
+                type='password' 
+                id='password-confirm' 
+                placeholder='confirme sua senha' 
+                name='password-confirm'
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+                ></input>
 
                 <span>A senha precisa ter no mínimo 1 algarismo</span>
                 <span>A senha precisa ter uma letra maiúscula</span>
