677993429853cb9dcfad4b4f24a26da2ce6e71c0
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-08-27 20:43:18 -0300

    starting working to login page

---
+++ share-books/src/pages/login/Login.js
@@ -0,0 +1,28 @@
+import './Login.css'
+
+const Login = () => {
+
+    return (
+        <div className='principal-div div-login'>
+
+            <section className='section-logo'>
+
+            </section>
+
+            <div className='div-login-data'>
+                <form>
+
+                    <label for=""></label>
+                    <input type='' name='' id='' placeholder=''></input>
+
+                    <label for=""></label>
+                    <input type='' name='' id='' placeholder=''></input>
+                    
+                    <button className='' type='submit'>Entrar</button>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+export default Login
