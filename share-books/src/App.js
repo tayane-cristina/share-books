07e2a4af07a5d5ad0f865b9394ff658ab26982a3
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-08-27 21:05:05 -0300

    adding some styles in the login page

--- share-books/src/App.js
+++ share-books/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 
 function App() {
   return (
     <div className="App">
       <h1>Share Books</h1>
-      <Register />
+      <Login />
     </div>
   );
 }
