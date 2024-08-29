3dee314c2a67e4587f66c145a7630f08e581d044
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-08-28 21:26:23 -0300

    starting working in the profile page

---
+++ share-books/src/pages/profile/Profile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div>
+            <aside>
+                <div className="user-infor">
+                    <img src="" alt="img-profile" className="img-profile"></img>
+                    <span className="user-name">Name</span>
+                    <p>Descrição...</p>
+                </div>
+
+                <div className="user-settings">
+                    
+                </div>
+                
+            </aside>
+
+            <section className="user-section">
+
+            </section>
+        </div>
+    )
+}
+export default Profile;
