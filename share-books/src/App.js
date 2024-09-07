dadb69934876ad0699b32db6e442bf7b4f04e0a8
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2024-09-07 17:49:08 -0300

    reset project

--- share-books/src/App.js
+++ share-books/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Login from './pages/login/Login';
-import Profile from './pages/profile/Profile';
-import Register from './pages/register/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='title-of-page'>Share Books</h1>
-      <Profile />
+  
     </div>
   );
 }
